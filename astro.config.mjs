414d23750c30a17e15cab3f0fabc7494af2a2393
Sercrac07 <sercassan@gmail.com>
2024-04-18 23:20:48 +0200

    Netlify dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // Creamos la configuración de Astro.
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 
 // Añadimos tailwind.
+import netlify from "@astrojs/netlify";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: "server",
+  adapter: netlify()
 });
